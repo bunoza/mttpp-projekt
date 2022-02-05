397d2b6b0f6ca9789e6ed2b0ec2cfe633275583f
Domagoj Bunoza <domagoj.bunoza@student.ferit.hr>
2022-02-05 18:01:53 +0100

    add LogInTest

---
+++ src/test/java/LogInTest.java
@@ -0,0 +1,61 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+
+
+public class LogInTest {
+
+    //-------------------Global Variables-----------------------------------
+    public WebDriver driver;
+    public String testURL = "https://www.honorbuy.com/";
+    Credentials credentials;
+
+    //----------------------Test Setup-----------------------------------
+    @BeforeMethod
+    public void setupTest() {
+        System.setProperty("webdriver.chrome.driver", "driver//chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.navigate().to(testURL);
+
+        try {
+            File file = new File("C:\\Users\\Domagoj\\Desktop\\credentials.txt");
+            Scanner myReader = new Scanner(file);
+            String[] input = new String[2];
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                input = data.split(",");
+            }
+            myReader.close();
+            credentials = new Credentials(input[0], input[1]);
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+    @Test
+    public void honorbuyLogInTest() throws InterruptedException {
+        driver.manage().window().maximize();
+        driver.findElement(By.className("login")).click();
+        driver.findElement(By.id("email")).clear();
+        driver.findElement(By.id("email")).sendKeys(credentials.getEmail());
+        driver.findElement(By.id("passwd")).clear();
+        driver.findElement(By.id("passwd")).sendKeys(credentials.getPassword());
+        driver.findElement(By.id("SubmitLogin")).click();
+        Assert.assertEquals(driver.findElement(By.xpath("//*[@id=\"header_user_info\"]/div[3]/a/span")).getText(), "Domagoj Bunoza");
+        ;
+    }
+    //---------------Test TearDown-----------------------------------
+    @AfterMethod
+    public void teardownTest() {
+        driver.quit();
+    }
+}
